fc78bdb2f60328882256a3eb19ce1448bd2e2a93
informatikum <informatikum@uni.com>
2025-10-22 16:59:00 +0200

    added field and robot into scene

--- bitbots_simulation/bitbots_mujoco_sim/main.py
+++ bitbots_simulation/bitbots_mujoco_sim/main.py
@@ -1,10 +1,19 @@
 import mujoco
 from mujoco import viewer
 
-m = mujoco.MjModel.from_xml_path("robot.urdf")
-d = mujoco.MjData(m)
-with viewer.launch_passive(m, d) as v:
-    print("Time\tPos X\tPos Y\tPos Z")
-    while v.is_running() and d.time < 5000.0:
-        print(f"{d.time:.2f}\t{d.qpos[0]:.4f}\t{d.qpos[1]:.4f}\t{d.qpos[2]:.4f}")
-        mujoco.mj_step(m, d)
+
+class Scene:
+    def __init__(self, path):
+        self.model = mujoco.MjModel.from_xml_path(path)
+        self.data = mujoco.MjData(self.model)
+
+    def launch(self):
+        with viewer.launch_passive(self.model, self.data) as view:
+            while view.is_running() and self.data.time < 10000.0:
+                mujoco.mj_step(self.model, self.data)
+
+
+field = Scene("xml/adult_field.xml")
+# goal = Scene("xml/goal.xml")
+
+field.launch()
