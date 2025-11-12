5e1058ad2fce8cf6c68f8716130d5f9d7115b7c7
Samim Maschal <samim.maschal@studium.uni-hamburg.de>
2025-11-12 18:36:34 +0100

    Start IMU logic

--- bitbots_simulation/bitbots_mujoco_sim/bitbots_mujoco_sim/Simulation.py
+++ bitbots_simulation/bitbots_mujoco_sim/bitbots_mujoco_sim/Simulation.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from mujoco import viewer
 from rclpy.node import Node
 from rclpy.time import Time
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Imu, JointState
 from bitbots_msgs.msg import JointCommand
 
 from bitbots_mujoco_sim.Robot import Robot
@@ -26,6 +26,8 @@ class Simulation(Node):
         self.step_number = 0
         self.ros_events_publish_frequency = 3
         self.js_publisher = self.create_publisher(JointState, "joint_states", 1)
+        # self.imu_frame = self.get_parameter("imu_frame").get_parameter_value().string_value ####
+        self.imu_publisher = self.create_publisher(Imu, "imu/data_raw", 1)
         self.create_subscription(JointCommand, "DynamixelController/command", self.joint_command_callback, 1)
 
     def add_controller(self, function) -> None:
@@ -60,6 +62,7 @@ class Simulation(Node):
 
     def publish_ros_events(self) -> None:
         self.publish_ros_joint_states_event()
+        self.publish_imu_event()
 
     def publish_ros_joint_states_event(self) -> None:
         js = JointState()
@@ -75,6 +78,33 @@ class Simulation(Node):
             js.velocity.append(joint.velocity)
             js.effort.append(self.data.actuator_force[joint.actuator_id])
         self.js_publisher.publish(js)
+
+    def publish_imu_event(self) -> None:
+        imu = Imu()
+        imu.header.stamp = Time(seconds=int(self.time), nanoseconds=int(self.time % 1 * 1e9)).to_msg()
+        # imu.header.frame_id = self.imu_frame #####
+        # change order because webots has different axis
+        accel_sensor = self.model.sensor("accelerometer")
+        adr = int(accel_sensor.adr)
+        dim = int(accel_sensor.dim)
+        accel_vals = self.data.sensordata[adr : adr + dim] #####
+        imu.linear_acceleration.x = accel_vals[0]
+        imu.linear_acceleration.y = accel_vals[1]
+        imu.linear_acceleration.z = accel_vals[2]
+        
+        # make sure that acceleration is not completely zero or we will get error in filter.
+        # Happens if robot is moved manually in the simulation
+        if imu.linear_acceleration.x == 0 and imu.linear_acceleration.y == 0 and imu.linear_acceleration.z == 0:
+            imu.linear_acceleration.z = 0.001
+
+        gyro_vels = self.data.sensordata[
+            int(self.model.sensor("gyro").adr) :
+            int(self.model.sensor("gyro").adr) + int(self.model.sensor("gyro").dim)] #####
+        imu.angular_velocity.x = gyro_vels[0]
+        imu.angular_velocity.y = gyro_vels[1]
+        imu.angular_velocity.z = gyro_vels[2]
+    
+        self.imu_publisher.publish(imu)
 
     def run(
         self,
